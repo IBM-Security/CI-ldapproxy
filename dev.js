e20c5e691908169e5760eff305fc5845b7033052
ibm-schmidtm <50636806+ibm-schmidtm@users.noreply.github.com>
2019-05-16 14:04:10 -0500

    Add files via upload

---
+++ dev.js
@@ -0,0 +1,15 @@
+// Dev environment for PPSdemo
+module.exports = {
+    tenant: {
+      ui: 'https://ppsdemo.ice.ibmcloud.com',
+      id: '35f095e9-65c6-4eab-9040-3c52ab6f3e3dxx',
+      secret: '3f6bN4mw66xx',
+      registry: 'bbdef01e-dce8-4fa4-b310-88b42b48169d'
+    },
+    ldap: {
+      port: 389,
+      root: 'o=pps',
+      type: 'uid='
+    },
+    log: "log"
+  };
